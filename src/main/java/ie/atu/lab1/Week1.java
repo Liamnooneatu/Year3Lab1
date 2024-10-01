2512b6bbeab015a9e183b1ec8d9e3d930162a191
G00420041@atu.ie <G00420041@atu.ie>
2024-10-01 10:57:03 +0100

    added name and age mapping

--- src/main/java/ie/atu/lab1/Week1.java
+++ src/main/java/ie/atu/lab1/Week1.java
@@ -1,6 +1,8 @@
 package ie.atu.lab1;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +12,18 @@ public class Week1 {
     {
         return "hello";
     }
+
+    @GetMapping("/greet/{name}")
+    public String name(@PathVariable String name )
+    {
+        return "Your name is " + name;
+    }
+
+    @GetMapping("/details")
+    public String age(@RequestParam String age1)
+    {
+        return "Your Age: " + age1;
+    }
+
+
 }
