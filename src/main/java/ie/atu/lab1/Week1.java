92d7c3690ffc04c3eda0b9d4739b5a37ae1d42ee
G00420041@atu.ie <G00420041@atu.ie>
2024-10-01 11:43:56 +0100

    started assignment 50% done

--- src/main/java/ie/atu/lab1/Week1.java
+++ src/main/java/ie/atu/lab1/Week1.java
@@ -8,22 +8,45 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Week1 {
     @GetMapping("/message")
-    public String message()
-    {
+    public String message() {
         return "hello";
     }
 
     @GetMapping("/greet/{name}")
-    public String name(@PathVariable String name )
-    {
+    public String name(@PathVariable String name) {
         return "Your name is " + name;
     }
 
     @GetMapping("/details")
-    public String age(@RequestParam String age1)
-    {
+    public String age(@RequestParam String age1) {
         return "Your Age: " + age1;
     }
 
+    int num3;
 
-}
+    @GetMapping("/calculate")
+    public int calculate(@RequestParam int num1, @RequestParam int num2, @RequestParam String operation) {
+
+
+        switch (operation) {
+            case "multiply":
+                return operation ": " + "Total: " + (num1 * num2);
+            break;
+
+            case "subtract":
+                return operation ": " + "Total: " + (num1 - num2);
+            break;
+
+            case "add":
+                return operation ": " + "Total: " + (num1 + num2);
+            break;
+
+            case "divide":
+                return operation ": " + "Total: " + (num1/num2);
+                }
+
+        }
+    }
+
+
+
